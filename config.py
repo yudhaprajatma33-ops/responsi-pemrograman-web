7eefebabebd1e52602885a78d32ecf682314b367
yudhaprajatma33-ops <yudhaprajatma33@gmail.com>
2026-07-21 19:14:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
+import os
+
 class Config:
-    """
-    Konfigurasi aplikasi Flask
-    """
-
-    # Konfigurasi Database MySQL
-    HOST = "dbkampus-students2212.d.aivencloud.com"
-    PORT = 26243
-    USER = "avnadmin"
-    PASSWORD = "AVNS_i3pU5bPDeWCHjlcSGo-"
-    DATABASE = "defaultdb"
-
+    """Konfigurasi aplikasi Flask"""
+    
+    # Konfigurasi Database MySQL (Membaca Environment Variables Vercel)
+    HOST = os.environ.get("DB_HOST", "dbkampus-students2212.d.aivencloud.com")
+    PORT = int(os.environ.get("DB_PORT", 26243))
+    USER = os.environ.get("DB_USER", "avnadmin")
+    PASSWORD = os.environ.get("DB_PASSWORD", "AVNS_i3pU5bPDeWCHjlcSGo-")
+    DATABASE = os.environ.get("DB_NAME", "defaultdb")
+    
     # Secret Key Flask
-    SECRET_KEY = "praktikum-flask-2026"
-
+    SECRET_KEY = os.environ.get("SECRET_KEY", "praktikum-flask-2026")
+    
     # Konfigurasi Flask
-    DEBUG = True
+    DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
